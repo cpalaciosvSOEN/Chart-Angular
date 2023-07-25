9521d437e694f6ef8e26acb18a3bcd6411683590
cpalaciosvSOEN <99422676+cpalaciosvSOEN@users.noreply.github.com>
2023-07-25 02:38:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chart-Angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/swimlane-line-chart-7ghg6c)
---
+++ app/data.ts
@@ -0,0 +1,47 @@
+export var multi = [
+  {
+    name: 'Valorizado',
+    series: [
+      {
+        name: '27-mar',
+        value: 1200,
+      },
+      {
+        name: '28-mar',
+        value: 1200,
+      },
+      {
+        name: '29-mar',
+        value: 1200,
+      },
+      {
+        name: '30-mar',
+        value: 1200,
+      },
+      {
+        name: '31-mar',
+        value: 1331.2,
+      },
+      {
+        name: '01-abr',
+        value: 1331.2,
+      },
+      {
+        name: '02-abr',
+        value: 1272.4,
+      },
+      {
+        name: '03-abr',
+        value: 1272.4,
+      },
+      {
+        name: '04-abr',
+        value: 9.149,
+      },
+      {
+        name: '05-abr',
+        value: 1272.4,
+      },
+    ],
+  },
+];
